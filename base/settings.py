894a72426990aa787957d1604f9f22050b30a0d8
golovachevegor <golovachev.e.b@gmail.com>
2025-08-23 16:12:47 +0300

    add models Post and Category

--- base/settings.py
+++ base/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'mptt',
+    'django_mptt_admin',
 ]
 
 MIDDLEWARE = [
@@ -104,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -115,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = (BASE_DIR / 'static')
+
+MEDIA_URL = '/media/'
+MIDIA_ROOT = (BASE_DIR / 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog.models import Post, Category
+from django_mptt_admin.admin import DjangoMpttAdmin
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(DjangoMpttAdmin):
+    """
+    Админ-панель модели категорий
+    """
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post)
