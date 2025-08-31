226ddb2e10515839656f323d6a0ad51526b224a5
golovachevegor <golovachev.e.b@gmail.com>
2025-08-31 15:43:15 +0300

    feat(accounts): add forms and urls

--- base/urls.py
+++ base/urls.py
@@ -23,6 +23,7 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('', include('accounts.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
