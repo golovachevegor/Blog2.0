4fe182a9ab7d873a97c06a839132898faa805f48
golovachevegor <golovachev.e.b@gmail.com>
2025-09-01 20:43:53 +0300

    chore(blog): add mixin and  message

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from blog.models import Post, Category
 from blog.forms import PostCreateForm, PostUpdateForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from services.mixins import AuthorRequiredMixin
 
 
 class PostFromCategory(ListView):
@@ -50,13 +53,14 @@ class PostDetailView(DetailView):
         return context
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     """
     Представление: создание материалов на сайте
     """
     model = Post
     template_name = 'blog/post_create.html'
     form_class = PostCreateForm
+    login_url = 'home'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -68,7 +72,7 @@ class PostCreateView(CreateView):
         return super().form_valid(form)
 
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(AuthorRequiredMixin, SuccessMessageMixin, UpdateView):
     """
     Представление: обновления материала на сайте
     """
@@ -76,6 +80,8 @@ class PostUpdateView(UpdateView):
     template_name = 'blog/post_update.html'
     context_object_name = 'post'
     form_class = PostUpdateForm
+    login_url = 'home'
+    success_message = 'Запись была успешно обновлена!'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -83,4 +89,5 @@ class PostUpdateView(UpdateView):
         return context
 
     def form_valid(self, form):
+        # form.instance.updater = self.request.user
         return super().form_valid(form)
